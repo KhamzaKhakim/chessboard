36be233b453758321fba200e12d2915cf7a90955
Khamza Khakim <hamzir.04@gmail.com>
2025-11-28 00:39:28 +0500

    Configure eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { hello } from "./utils/hello.js";
-
-function initialCommit() {
-  return "Hello";
+function initialCommit(name: string) {
+  return `Hello ${name}`;
 }
 
-console.log(initialCommit());
-console.log(hello("John"));
+console.log(initialCommit("John"));
