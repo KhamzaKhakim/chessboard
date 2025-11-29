49e194ef5d741c9c8bbfc6222a0c381e18f69706
Khamza Khakim <hamzir.04@gmail.com>
2025-11-30 02:22:49 +0500

    Start moving pieces

--- src/chessboard.ts
+++ src/chessboard.ts
@@ -7,31 +7,63 @@ export class Chessboard {
   size: number;
   fen: string;
   svgPieces: Record<string, HTMLImageElement> | null = null;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
 
-  constructor(ref: string, size: number, fen: string = startPositon) {
+  constructor(ref: string, size: number = 8, fen: string = startPositon) {
     this.ref = ref;
     this.size = size;
     this.fen = fen;
-  }
 
-  async drawBoard() {
-    const canvas = document.getElementById(this.ref) as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d")!;
+    const canvas = document.getElementById(this.ref);
 
-    const TILE = canvas.width / this.size;
-
-    for (let r = 0; r < this.size; r++) {
-      for (let c = 0; c < this.size; c++) {
-        ctx.fillStyle = (r + c) % 2 === 0 ? "#f0d9b5" : "#b58863";
-        ctx.fillRect(c * TILE, r * TILE, TILE, TILE);
-      }
+    if (!(canvas instanceof HTMLCanvasElement)) {
+      throw new Error(`Element #${ref} is not a canvas`);
     }
 
-    //preloadPieces;
-    if (!this.svgPieces || !this.svgPieces.size) {
+    this.canvas = canvas;
+
+    this.ctx = canvas.getContext("2d")!;
+  }
+
+  async init() {
+    await this.setup();
+    this.draw();
+  }
+
+  async setup() {
+    //preload pieces;
+    if (!this.svgPieces) {
       this.svgPieces = await preloadPieces(pieces);
     }
 
-    drawPiecesFromFen(this.fen, ctx, TILE, this.svgPieces);
+    //add event listeneres for canvas
+    this.canvas.onmousedown = this.mouseDownHandler;
+  }
+
+  draw() {
+    if (!this.svgPieces) {
+      throw new Error("Preloaded images not found");
+    }
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    const TILE = this.canvas.width / this.size;
+
+    for (let r = 0; r < this.size; r++) {
+      for (let c = 0; c < this.size; c++) {
+        this.ctx.fillStyle = (r + c) % 2 === 0 ? "#f0d9b5" : "#b58863";
+        this.ctx.fillRect(c * TILE, r * TILE, TILE, TILE);
+      }
+    }
+
+    drawPiecesFromFen(this.fen, this.ctx, TILE, this.svgPieces);
+  }
+
+  private mouseDownHandler(e: MouseEvent) {
+    //i need pieces
+    e.preventDefault();
+    const x = e.offsetX;
+    const y = e.offsetY;
+    console.log(`Clicked at canvas position: ${x}, ${y}`);
   }
 }
